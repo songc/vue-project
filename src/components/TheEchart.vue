f5e092f40ff77b9ea1d495ea6aee7a96d1a7d2d1
songchaodevip <songcmail@gmail.com>
2017-12-13 01:40:26 +0800

    修改计算特征方法的中的一些bug

--- src/components/TheEchart.vue
+++ src/components/TheEchart.vue
@@ -3,7 +3,8 @@
   <div id="the-echart" ref="theEchart">
   </div>
   <Button @click="smooth(10,optionsData)">Smooth</Button>
-  <Modal title="add comment" v-model="modal" @on-cancel="false" @on-ok="addMark(params)">
+  <Button @click="clearMarkPoint">ClearMarkPoint</Button>
+  <Modal :title="isStartPoint? 'add a startPoint':'add a endPonint'" v-model="modal" @on-cancel="modal=false" @on-ok="addMark(params)">
     <Input v-model="comment"></Input>
   </Modal>
 </div>
@@ -11,6 +12,7 @@
 </template>
 
 <script>
+import iecCal from '../util/iecCal.js'
 import analysisApi from '../api/analysis'
 import echarts from 'echarts'
 export default {
@@ -18,14 +20,84 @@ export default {
   data() {
     return {
       modal: false,
+      isStartPoint: true,
       comment: '',
       chart: null,
-      params: null
+      params: null,
+      startPoint: {
+        value: null,
+        dataIndex: null,
+        seriesIndex: null,
+        name: null
+      },
+      endPoint: {
+        value: null,
+        dataIndex: null,
+        seriesIndex: null,
+        name: null
+      },
+      wareArray: []
     }
   },
   computed: {
     optionsData: function() {
       return this.$store.getters.getData(1)
+    },
+    ware() {
+      return {
+        start: this.startPoint,
+        end: this.endPoint
+      }
+    },
+    wareArrayData() {
+      return this.wareArray.map(wave => {
+        let seriesIndex = wave.start.seriesIndex
+        let startIndex = wave.start.dataIndex
+        let endIndex = wave.end.dataIndex
+        return this.optionsData.data[seriesIndex].slice(startIndex, endIndex).map(x => parseFloat(x))
+      })
+    },
+    xNumber() {
+      return this.wareArrayData.map(array => iecCal.calXNumber(array))
+    },
+    meanValue() {
+      return this.wareArrayData.map(array => iecCal.calMeanValue(array))
+    },
+    subMean() {
+      return this.wareArrayData.map((array, index) => iecCal.calSubMean(array, this.meanValue[index]))
+    },
+    deviation() {
+      return this.subMean.map(array => iecCal.calDeviation(array))
+    },
+    skewness() {
+      return this.subMean.map((array, index) => iecCal.calSkewness(array, this.deviation[index]))
+    },
+    kurtosis() {
+      return this.subMean.map((array, index) => iecCal.calKurtosis(array, this.deviation[index]))
+    },
+    activity() {
+      return this.wareArrayData.map(array => iecCal.calActivity(array))
+    },
+    diff() {
+      return this.wareArrayData.map(array => iecCal.calDiff(array))
+    },
+    mobility() {
+      return this.diff.map((array, index) => iecCal.calMobility(array, this.activity[index]))
+    },
+    complexity() {
+      return this.diff.map((array, index) => iecCal.calComplexity(array, this.mobility[index]))
+    },
+    time() {
+      return this.wareArrayData.map(array => iecCal.calTime(array, 1))
+    },
+    slop() {
+      return this.wareArrayData.map(array => iecCal.calSlope(array))
+    },
+    y() {
+      return this.wareArrayData.map(array => iecCal.calY(array))
+    },
+    area() {
+      return this.subMean.map(array => iecCal.calArea(array))
     }
   },
   watch: {
@@ -38,15 +110,6 @@ export default {
   },
   mounted() {
     this.chart = echarts.init(this.$refs.theEchart)
-    this.chart.setOption({
-      title: {text: 'example'},
-      tooltip: {},
-      xAxis: {
-        data: []
-      },
-      yAxis: {},
-      series: []
-    })
     let _this = this
     this.chart.on('click', function(params) {
       _this.modal = true
@@ -55,6 +118,28 @@ export default {
   },
   methods: {
     addMark(params) {
+      if (this.isStartPoint) {
+        this.startPoint = {
+          value: params.value,
+          dataIndex: params.dataIndex,
+          name: params.name,
+          seriesIndex: params.seriesIndex
+        }
+        this.isStartPoint = !this.isStartPoint
+      } else {
+        if (this.validEndPoint(params)) {
+          this.endPoint = {
+            value: params.value,
+            dataIndex: params.dataIndex,
+            name: params.name,
+            seriesIndex: params.seriesIndex
+          }
+          this.isStartPoint = !this.isStartPoint
+          this.wareArray.push(this.ware)
+        } else {
+          return false
+        }
+      }
       let mySeries = this.chart.getOption().series
       mySeries[params.seriesIndex].markPoint.data.push({
         name: this.comment,
@@ -63,6 +148,35 @@ export default {
       this.chart.setOption({
         series: mySeries
       })
+    },
+    clearMarkPoint() {
+      let mySeries = this.chart.getOption().series
+      mySeries.forEach(serie => {
+        serie.markPoint.data = []
+      })
+      this.chart.setOption({
+        series: mySeries
+      })
+      this.wareArray = []
+    },
+    validEndPoint(params) {
+      if (params.seriesIndex === this.startPoint.seriesIndex) {
+        if (params.dataIndex > this.startPoint.dataIndex) {
+          return true
+        } else {
+          this.$Notice.warning({
+            title: 'error',
+            desc: 'endPonint must be in the after of startPonit'
+          })
+          return false
+        }
+      } else {
+        this.$Notice.warning({
+          title: 'error',
+          desc: 'endPoint and startPonit must be in the similar channl'
+        })
+        return false
+      }
     },
     smooth(windowsWidth, optionsData) {
       let data = optionsData
@@ -119,8 +233,14 @@ export default {
               data: x,
               showSymbol: false,
               markPoint: {
-                symbol: 'pin',
-                symbolSize: '30',
+                symbol: 'circle',
+                symbolSize: 15,
+                label: {
+                  normal: {
+                    show: true,
+                    position: 'top'
+                  }
+                },
                 data: []
               }
             }
--- src/util/iecCal.js
+++ src/util/iecCal.js
@@ -5,24 +5,28 @@ const iecUtil = {
   calMeanValue(array) {
     return array.reduce((a, b) => a + b, 0) / array.length
   },
-  calDeviation(array, meanValue) {
+  calSubMean(array, meanValue) {
     return array.map(x => x - meanValue)
-      .reduce((a, b) => a * b, 1) / (array.length - 1)
   },
-  calSkewness(array, meanValue, deviation) {
-    let sumCube = array.map(x => Math.pow(x - meanValue, 3)).reduce((a, b) => a + b, 0)
-    return sumCube / ((array.length - 1) * Math.pow(deviation, 3))
+  calDeviation(subMean) {
+    return subMean.map((value, index, array) => {
+      return index === array.length - 1 ? 0 : array[index + 1] * value
+    }).reduce((a, b) => a + b, 0) / (subMean.length - 1)
   },
-  calKurtosis(array, meanValue, deviation) {
-    let sum = array.map(x => x - meanValue).map(x => Math.pow(x, 4)).reduce((a, b) => a + b, 0)
-    return sum / ((array.length - 1) * Math.pow(deviation, 4))
+  calSkewness(subMean, deviation) {
+    let sumCube = subMean.map(x => Math.pow(x, 3)).reduce((a, b) => a + b, 0)
+    return sumCube / ((subMean.length - 1) * Math.pow(deviation, 3))
+  },
+  calKurtosis(subMean, deviation) {
+    let sum = subMean.map(x => Math.pow(x, 4)).reduce((a, b) => a + b, 0)
+    return sum / ((subMean.length - 1) * Math.pow(deviation, 4))
   },
   calActivity(array) {
     return array.map(x => Math.pow(x, 2)).reduce((a, b) => a + b, 0) / array.length
   },
   calDiff(array) {
     let diff = array.map((value, index, array) => {
-      return index === array.length ? 0 : (array[index + 1] - value) * value
+      return index === array.length - 1 ? 0 : (array[index + 1] - value) * value
     })
     diff.pop()
     return diff
@@ -31,7 +35,8 @@ const iecUtil = {
     return this.calMeanValue(diff) / activity
   },
   calComplexity(diff, mobility) {
-    return this.calMeanValue(this.diff(diff)) / (diff - mobility)
+    let result = (this.calMeanValue(diff)) / (this.calMeanValue(diff) - mobility)
+    return Math.sqrt(Math.abs(result))
   },
   calTime(array, rate) {
     return array.length / rate
